840461468f28537994d99076bd358e7a87676514
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-06 16:35:12 +0530

    The player polls the die to get a numger b/n 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,6 +5,7 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         int player1Position=0;
         System.out.println("Game begins: player1 at position: "+player1Position);
-
+        int player1Dice = (int)(Math.floor(Math.random() * 10) % 6) + 1;
+        System.out.println("Dice rolled value: " + player1Dice);
     }
 }
